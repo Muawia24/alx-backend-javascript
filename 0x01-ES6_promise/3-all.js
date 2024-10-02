88baf8bc21b79ca305de198172c468e6e752dc4f
Ahmed Muawia <amuawia666@gmail.com>
2024-10-02 20:54:24 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup() {
     .then(([photoResponse, userResponse]) => {
       console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
-    .catch(() => new Error('Signup system offline'));
+    .catch(() => console.error('Signup system offline'));
 }
