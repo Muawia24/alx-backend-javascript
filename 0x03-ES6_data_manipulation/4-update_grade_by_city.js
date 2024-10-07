1f5964505c87dc68518dffdb1ad9e85bafef9bf6
Ahmed Muawia <amuawia666@gmail.com>
2024-10-07 22:32:43 +0300

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+export default function updateStudentGradeByCity(listObjs, city, newGrades) {
+  if (typeof listObjs !== 'object') {
+    return [];
+  }
+
+  const student = listObjs.filter((obj) => obj.location === city);
+
+  const studentGrad = student.map((student) => {
+    for (const gradeObj of newGrades) {
+      if (student.id === gradeObj.studentId) {
+        student.grade = gradeObj.grade; // eslint-disable-line no-param-reassign
+      }
+      if (gradeObj.grade === 'undefined') {
+        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+      }
+    }
+    return student;
+  });
+  return studentGrad;
+}
