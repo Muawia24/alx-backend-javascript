9022b2bb7e4bb2315e3f32ded6a6f2a672a5ad25
Ahmed Muawia <amuawia666@gmail.com>
2024-12-01 12:57:49 +0200

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,21 +4,28 @@ const calculateNumber = require('./1-calcul');
 describe('calculateNumber', () => {
   it('should return sum of integers', () => {
     chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    chai.expect(calculateNumber('SUM', -1.4, 4.5)).to.equal(4);
+  });
+});
+
+describe('sUBTRACT', () => {
+  it('should return the difference of two rounded numbers', () => {
+    chai.expect(calculateNumber('SUBTRACT', 5.4, 2.7)).to.equal(2);
   });
 
-  it('should return the rounded number if only one is provided', () => {
-    chai.expect(calculateNumber('SUM', 2)).to.equal(2);
-    chai.expect(calculateNumber('SUBTRACT', 2.7)).to.equal(3);
+  it('should handle negative results', () => {
+    chai.expect(calculateNumber('SUBTRACT', 3.4, 5.6)).to.equal(-3);
+  });
+});
+
+describe('dIVIDE', () => {
+  it('should return the division of two rounded numbers', () => {
+    chai.expect(calculateNumber('DIVIDE', 8.5, 2.2)).to.equal(4.5);
   });
 
-  it('should cast non-numbers into numbers', () => {
-    chai.expect(calculateNumber('SUM', true, 3)).to.equal(4);
+  it('should return "Error" for division by zero', () => {
+    chai.expect(calculateNumber('DIVIDE', 8.4, 0.2)).to.equal('Error');
   });
-
-
   it('should throw typeerror if either param cannot be coerced to a number', () => {
     chai.expect(() => calculateNumber('hello', 1.0, 2.4)).to.throw();
   });
